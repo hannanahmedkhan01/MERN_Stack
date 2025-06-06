4661efd143b36ddcd8ce1e9bbe472a3094d5098c
hannanahmedkhan01 <hannan.khan010101@gmail.com>
2025-06-06 14:34:42 +0500

    JWT Auth first update

---
+++ models/userModels.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter name.."],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter email.."],
+      unique: true,
+    },
+    Password: {
+      type: String,
+      required: [true, "Please enter Password.."],
+    },
+  },
+  {
+    Timestamps: true,
+  }
+
+);
+
+
+module.exports=mongoose.model('User, userSchema')
